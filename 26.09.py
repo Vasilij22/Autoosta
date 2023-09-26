3b76f494a1631a9bad40e180c285dbe2f3ea4738
Vasilij22 <91057254+Vasilij22@users.noreply.github.com>
2023-09-26 20:52:22 +0300

    Create 26.09.py

---
+++ 26.09.py
@@ -0,0 +1,10 @@
+import PySimpleGUI as psg
+
+class info():
+    def __init__(self,stacijas,atbraukšanas_laiks,izbraukšanas_laiks,cena,visa_ceļa_laiks,autobusa_numurs):
+        self.stacijas = stacijas
+        self.atbraukšanas_laiks = atbraukšanas_laiks
+        self.izbraukšanas_laiks = izbraukšanas_laiks
+        self.cena = cena
+        self.visa_ceļa_laiks = visa_ceļa_laiks
+        self.autobusa_numura = autobusa_numurs
